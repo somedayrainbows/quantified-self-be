d22786990dedf84a8dc9b4f6c531166f01563a37
YOUR NAME <ebassity@gmail.com>
2017-07-02 16:38:40 -0600

    Finishes all foods endpoint

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var Food = require("./lib/models/food")
-var md5 = require('md5')
 
-
-app.set('port', process.env.PORT || 7878)
+app.set('port', process.env.PORT || 3000)
 app.locals.title = "Quantified Self"
 app.locals.foods = {
   name: 'Banana'
@@ -17,40 +15,26 @@ app.get('/', function(request, response) {
   response.send(app.locals.title)
 })
 
-app.get('/api/foods/:id', function(request, response) {
+app.get('/api/v1/foods/:id', function(request, response) {
   var id = request.params.id
   Food.find(id)
   .then(function(data) {
     if (data.rowCount == 0) { return response.sendStatus(404) }
-      response.json(data.row[0])
+      response.json(data.rows[0])
   })
 })
 
-app.post('/api/foods', function(request, response) {
+app.post('/api/v1/foods', function(request, response) {
   var id = Date.now()
   var name = request.body.name
-  // var name = request.body.name
-  // var calories = request.body.calories
-  // var created_at = request.body.created_at
-  // var message = request.data.rows[data.rows.length]
-  // var id = md5(message)
-  // example usage for md5:
-  // var msg = “super secret code”;
-  // var hash = md5(msg);
 
   if (!name) {
     return response.status(422).send({
       error: 'No name property provided'
     })
   }
-  // } else if (!calories) {
-  //   return response.status(422).send({
-  //     error: 'No calories property provided'
-  //   })
-  // } else {
-  //   id = md5(message)
-    app.locals.foods[id] = name
-    response.status(201).json({ id, name } )
+  app.locals.foods[id] = name
+  response.status(201).json({ id, name } )
 })
 
 if(!module.parent) {
--- test/server-test.js
+++ test/server-test.js
@@ -4,14 +4,12 @@ var request = require('request')
 var Food = require('../lib/models/food')
 
 describe('Server', function() {
-
   before(function(done) {
     this.port = 7878
     this.server = app.listen(this.port, function(error, result) {
       if (error) { return done(error) }
       done()
     })
-
     this.request = request.defaults({
       baseUrl: 'http://localhost:7878'
     })
@@ -24,92 +22,90 @@ describe('Server', function() {
   it('exists', function() {
     assert(app)
   })
-})
 
-describe('GET /', function() {
 
-  it('returns a 200', function(done) {
-    this.request.get('/', function(error, response) {
-      if (error) { done(error) }
-      assert.equal(response.statusCode, 200)
-      done()
+  describe('GET /', function() {
+    it('returns a 200', function(done) {
+      this.request.get('/', function(error, response) {
+        if (error) { done(error) }
+        assert.equal(response.statusCode, 200)
+        done()
+      })
     })
   })
 
-  it('has a body with the name of the application', function(done) {
-    var title = app.locals.title
+  describe('GET /api/v1/foods/:id', function() {
+    this.timeout(10000000)
 
-    this.request.get('/', function(error, response) {
-      if (error) { done(error) }
-      assert(response.body.includes(title), `"${response.body}" does not include "${title}".`)
-      done()
-    })
-  })
-})
-
-describe('POST /api/foods', function() {
-  beforeEach(function() {
-    app.locals.foods = {}
-  })
-  it('does not return a 404', function(done) {
-    this.request.post('/api/foods', function(error, response) {
-      if (error) { done(error) }
-      assert.notEqual(response.statusCode, 404)
-      done()
-    })
-  })
-
-  it('should receive and store data', function(done) {
-    var foods = {
-      name: 'Pineapples'
-    }
-    this.request.post('/api/foods', { form: foods }, function(error, response) {
-      if (error) { done(error) }
-
-      var foodCount = Object.keys(app.locals.foods).length
-
-      assert.equal(foodCount, 1)
-      done()
-    })
-  })
-})
-
-describe('GET /api/foods/:id', function() {
-  this.timeout(10000000)
-  beforeEach(function(done) {
-    Food.createFood("Banana", 15, new Date())
-    .then(function() { done() })
+    beforeEach(function(done) {
+      Food.createFood("Banana", 15, new Date())
+      .then(function() {
+        Food.createFood("Omelette", 230, Date())
+        .then(function() { done() })
+      })
     })
 
-    afterEach(function(done) {
-      Food.emptyFoodsTable().then(function() { done()
+      afterEach(function(done) {
+        Food.emptyFoodsTable().then(function() { done() })
     })
-  })
 
-  it('returns a 404 if the resource is not found', function(done) {
-    this.request.get('/api/foods/456', function(error, response) {
-      if (error) { done(error) }
-      assert.equal(response.statusCode, 404)
-      done()
+    it('returns a 404 if the resource is not found', function(done) {
+      this.request.get('/api/v1/foods/456', function(error, response) {
+        if (error) { done(error) }
+        assert.equal(response.statusCode, 404)
+        done()
+      })
     })
-  })
 
-  it('has the id, name and calories from the resource', function(done) {
-    var ourRequest = this.request
-    Food.findFirst()
-    .then(function(data) {
-      var id = data.rows[0].id
-      var name = data.rows[0].name
-      var calories = data.rows[0].calories
-      var created_at = data.rows[0].created_at
-      ourRequest.get('/api/secrets/' + id, function(error, response) {
+    it('has the id, name and calories from the resource', function(done) {
+      var id = 1
+
+      this.request.get('/api/v1/foods/' + id, function(error, response) {
         if (error) { done(error) }
 
         var parsedFood = JSON.parse(response.body)
+
         assert.equal(parsedFood.id, id)
-        assert.equal(parsedFood.name, name)
-        assert.equal(parsedFood.calories, calories)
+        assert.equal(parsedFood.name, "Banana")
+        assert.equal(parsedFood.calories, 15)
         assert.ok(parsedFood.created_at)
+        done()
+      })
+    })
+
+    it('has a body with the name of the application', function(done) {
+      var title = app.locals.title
+
+      this.request.get('/', function(error, response) {
+        if (error) { done(error) }
+        assert(response.body.includes(title), `"${response.body}" does not include "${title}".`)
+        done()
+      })
+    })
+  })
+
+  describe('POST /api/v1/foods', function() {
+    beforeEach(function() {
+      app.locals.foods = {}
+    })
+    it('does not return a 404', function(done) {
+      this.request.post('/api/v1/foods', function(error, response) {
+        if (error) { done(error) }
+        assert.notEqual(response.statusCode, 404)
+        done()
+      })
+    })
+
+    it('should receive and store data', function(done) {
+      var foods = {
+        name: 'Pineapples'
+      }
+      this.request.post('/api/v1/foods', { form: foods }, function(error, response) {
+        if (error) { done(error) }
+
+        var foodCount = Object.keys(app.locals.foods).length
+
+        assert.equal(foodCount, 1)
         done()
       })
     })
