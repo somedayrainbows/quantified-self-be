ad140a0a586009e1947f30403ce3408ff141b6dc
YOUR NAME <ebassity@gmail.com>
2017-07-08 17:52:02 -0600

    Extracts routes into their own files
    * creates router files for meals and foods
    * cleans up server.js

---
+++ routes/foods.js
@@ -0,0 +1,74 @@
+var express = require('express')
+var router = express.Router()
+var Food = require('../lib/models/food')
+
+router.get('/api/v1/foods', function(request, response) {
+  Food.all()
+    .then(function(data) {
+      if(data.rowCount == 0) {
+        return response.status(422).send({ error: 'No records found' })
+      }
+      response.json(data.rows)
+    })
+})
+
+router.get('/api/v1/foods/:id', function(request, response) {
+  var id = request.params.id
+
+  Food.find(id)
+    .then(function(data) {
+      if(data.rowCount == 0) { return response.sendStatus(404) }
+      response.json(data.rows[0])
+    })
+})
+
+router.post('/api/v1/foods', function(request, response) {
+  var name = request.body.name
+  var calories = request.body.calories
+
+  // if (!name) {
+  //   return response.status(422).send({
+  //     error: 'No name property provided!'
+  //   })
+  // }
+
+  Food.createFood(name, calories)
+  .then(function() {
+    Food.findByName(name)
+    .then(function(data) {
+      response.json(data.rows[0])
+    })
+  })
+
+  //   if (error)
+  //   response.send(error)
+  //   response.json({ message: 'Food created!' })
+  //   response.status(201).json({ id, name })
+})
+
+router.put('/api/v1/foods/:id', function (request, response) {
+  var id = request.params.id
+  var payload = request.body
+
+  Food.update(id, payload)
+    .then(function() {
+      Food.find(id)
+        .then(function(data) {
+          response.json(data.rows[0])
+        })
+    })
+})
+
+router.delete('/api/v1/foods/:id', function (request, response) {
+  var id = request.params.id
+
+  Food.deactivate(id)
+    .then(function() {
+      Food.all()
+        .then(function(data) {
+          response.json(data.rows)
+        })
+    })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-var Food = require('./lib/models/food')
-var Meal = require('./lib/models/meal')
 
 app.set('port', process.env.PORT || 7878);
 app.locals.title = "Quantified Self"
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(require('./routes/foods'))
+app.use(require('./routes/meals'))
+
 app.use(function(request, response, next) {
   console.log('Something is happening...')
   next()
@@ -18,84 +20,7 @@ app.get('/', function(request, response) {
   response.send(app.locals.title);
 })
 
-app.get('/api/v1/foods', function(request, response) {
-  Food.all()
-    .then(function(data) {
-      if(data.rowCount == 0) {
-        return response.status(422).send({ error: 'No records found' })
-      }
-      response.json(data.rows)
-    })
-})
 
-app.get('/api/v1/foods/:id', function(request, response) {
-  var id = request.params.id
-
-  Food.find(id)
-    .then(function(data) {
-      if(data.rowCount == 0) { return response.sendStatus(404) }
-      response.json(data.rows[0])
-    })
-})
-
-app.post('/api/v1/foods', function(request, response) {
-  var name = request.body.name
-  var calories = request.body.calories
-
-  // if (!name) {
-  //   return response.status(422).send({
-  //     error: 'No name property provided!'
-  //   })
-  // }
-
-  Food.createFood(name, calories)
-  .then(function() {
-    Food.findByName(name)
-    .then(function(data) {
-      response.json(data.rows[0])
-    })
-  })
-
-  //   if (error)
-  //   response.send(error)
-  //   response.json({ message: 'Food created!' })
-  //   response.status(201).json({ id, name })
-})
-
-app.put('/api/v1/foods/:id', function (request, response) {
-  var id = request.params.id
-  var payload = request.body
-
-  Food.update(id, payload)
-    .then(function() {
-      Food.find(id)
-        .then(function(data) {
-          response.json(data.rows[0])
-        })
-    })
-})
-
-app.delete('/api/v1/foods/:id', function (request, response) {
-  var id = request.params.id
-
-  Food.deactivate(id)
-    .then(function() {
-      Food.all()
-        .then(function(data) {
-          response.json(data.rows)
-        })
-    })
-})
-
-app.get('/api/v1/meals/:id', function(request, response) {
-  var id = request.params.id
-
-  Meal.find(id)
-    .then(function(data) {
-      if(data.rowCount == 0) { return response.sendStatus(404) }
-      response.json(data.rows[0])
-    })
-})
 
 if(!module.parent) {
   app.listen(app.get('port'), function() {
